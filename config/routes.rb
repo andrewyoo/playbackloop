440d62d78fd1eb9988fbc562d2ea1913a9db42a6
Andrew Yoo <andrew@phacade.com>
2017-12-05 02:44:50 +0800

    cookies and playlist enter

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
-  resources :playlists, only: [:show]
+  resources :playlists, only: [:show, :create]
   get '/auth/:provider/callback', to: 'sessions#create'
 end
