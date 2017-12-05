376dded8f7bc72d00fdeaf0e699926ea53e5cc59
Andrew Yoo <andrew@phacade.com>
2017-12-05 16:16:02 +0800

    secrets, cookies, analytics

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,3 @@ secrets = Rails.application.secrets
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, secrets.google[:client_id], secrets.google[:secret], scope: 'userinfo.profile,userinfo.email,youtube.readonly'
 end
-
