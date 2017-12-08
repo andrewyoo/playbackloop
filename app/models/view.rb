74ea348daade65d5ce40a7be5601fe87cb8ee94f
Andrew Yoo <andrew@phacade.com>
2017-12-08 15:08:23 +0800

    improve recent and current playlist views

--- app/models/view.rb
+++ app/models/view.rb
@@ -3,6 +3,7 @@ class View < ApplicationRecord
   
   scope :recently_played, -> { order(updated_at: :desc) }
   scope :not_user, ->(user) { where.not(user_id: user.try(:id)) }
+  scope :with_user, ->(user) { where(user_id: user.try(:id))}
   
   simple_enum :list_type, %i[ playlist channel ]
 end
