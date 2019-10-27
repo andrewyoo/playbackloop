0d4c51e040eb058a455f525ec38c9f69d2610e23
Andrew Yoo <andrew@phacade.com>
2019-10-27 01:27:44 -0700

    show more recently played in accounts and allow removal

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,7 +2,7 @@ class AccountsController < ApplicationController
   before_action :require_login
   
   def show
-    @current_playlists = current_playlists
+    @current_playlists = current_playlists(12)
     @user_playlists = user_playlists
     @channel_subscriptions = channel_subscriptions
   end
